c138a8206328bf30b8a1b25eb7c47f84e7f01368
siven <sivenzsw@softspeed.com.cn>
2018-05-08 22:43:16 +0800

    js第一章完成

---
+++ javascript/p1/js_p1.js
@@ -0,0 +1,8 @@
+function poem() {
+    
+    document.write("<h3>春晓</h3>");
+    document.write("<p>春眠不觉晓,</p>");
+    document.write("<p>处处闻啼鸟。</p>");
+    document.write("<p>夜来风雨声,</p>");
+    document.write("<p>花来知多少。</p>");
+}
