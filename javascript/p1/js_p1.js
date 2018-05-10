24c1ccef1c719f4a196f49c13932bb3482114d7a
siven <sivenzsw@softspeed.com.cn>
2018-05-10 19:35:49 +0800

    增加语句与注释

--- javascript/p1/js_p1.js
+++ javascript/p1/js_p1.js
@@ -1,8 +1,10 @@
 function poem() {
-    
-    document.write("<h3>春晓</h3>");
-    document.write("<p>春眠不觉晓,</p>");
-    document.write("<p>处处闻啼鸟。</p>");
-    document.write("<p>夜来风雨声,</p>");
+    /* 一首家喻户晓的唐诗 
+        小学课本选录，读起来朗朗上口
+    */
+    document.write("<h3>春晓</h3>")
+    document.write("<p>春眠不觉晓,</p>")
+    document.write("<p>处处闻啼鸟。</p>"); document.write("<p>夜来风雨声,</p>");
     document.write("<p>花来知多少。</p>");
+    document.write("<h5>唐诗三百首之一</h5>");
 }
